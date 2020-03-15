d2eb3a8dac0f670400c31ada0b327c81d6a1ef65
Joseph Hammad <joseph.hammad@epitech.eu>
2020-03-15 11:34:50 +0100

    [FEAT] activities section

--- src/app/destination/destination.component.ts
+++ src/app/destination/destination.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IDestination } from './destination.model';
+import { ActivityService } from '../activity/activity.service';
+import { Observable } from 'rxjs';
+import { IActivity } from '../activity/activity.model';
 
 @Component({
 	selector: 'app-destination',
@@ -9,14 +12,17 @@ import { IDestination } from './destination.model';
 })
 export class DestinationComponent implements OnInit {
 	destination: IDestination;
+	activities$: Observable<IActivity[]>;
 	get bgImg() { return `url('${this.destination.bg}')`; }
 	constructor(
 		protected route: ActivatedRoute,
+		protected activityService: ActivityService,
 	) {}
 	ngOnInit() {
 		this.route.data
 		.subscribe((data: { destination: IDestination }) => {
 			this.destination = data.destination;
+			this.activities$ = this.activityService.getActivitiesByDestination(data.destination.id);
 		});
 	}
 }
